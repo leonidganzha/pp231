cccd68dde45e5aa68216bb42c4b3c19f6a2fc260
leonidganzha <leonidganzha@gmail.com>
2022-11-28 23:09:37 +0400

    fixed @Transactional annotation

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -8,7 +8,7 @@ import web.model.User;
 import java.util.List;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
 
     private final UserDao userDao;
@@ -17,16 +17,19 @@ public class UserServiceImpl implements UserService {
         this.userDao = userDao;
     }
 
+    @Transactional
     @Override
     public void addUser(User user) {
         userDao.addUser(user);
     }
 
+    @Transactional
     @Override
     public void updateUser(User user) {
         userDao.updateUser(user);
     }
 
+    @Transactional
     @Override
     public void removeUser(int id) {
         userDao.removeUser(id);
